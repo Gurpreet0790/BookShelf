deff89d81df4202f321a1305d75c7adc4b95b3e9
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-01-25 11:12:08 -0800

    Initial Commit

---
+++ BookShelfApp/BookShelfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfAppApp.swift
+//  BookShelfApp
+//
+//  Created by ReetDhillon on 2024-01-25.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
